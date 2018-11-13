51b38065472b0eefbedc42e12119f09f050650ac
Naveen Jain <nveenjain@gmail.com>
2018-11-14 01:01:27 +0530

    add fft, dft

--- recFFT.cpp
+++ recFFT.cpp
@@ -3,47 +3,61 @@ using namespace std;
   
 typedef complex<double> cd; 
   
-vector<cd> fft(vector<cd>& a) 
-{ 
-    int n = a.size(); 
-  
-    if (n == 1) 
-        return vector<cd>(1, a[0]); 
-  
-    vector<cd> w(n); 
-    for (int i = 0; i < n; i++) { 
-        double alpha = 2 * M_PI * i / n; 
-        w[i] = cd(cos(alpha), sin(alpha)); 
-    } 
-  
-    vector<cd> A0(n / 2), A1(n / 2); 
-    for (int i = 0; i < n / 2; i++) { 
-  
-        A0[i] = a[i * 2]; 
-  
-        A1[i] = a[i * 2 + 1]; 
-    } 
-  
-    vector<cd> y0 = fft(A0);  
-  
-    vector<cd> y1 = fft(A1); 
-  
-    vector<cd> y(n); 
-  
-    for (int k = 0; k < n / 2; k++) { 
-        y[k] = y0[k] + w[k] * y1[k]; 
-        y[k + n / 2] = y0[k] - w[k] * y1[k]; 
-    } 
-    return y; 
-} 
+using cd = complex<double>;
+const double PI = acos(-1);
+
+void fft(vector<cd> & a, bool invert) {
+    int n = a.size();
+    if (n == 1)
+        return;
+
+    vector<cd> a0(n / 2), a1(n / 2);
+    for (int i = 0; 2 * i < n; i++) {
+        a0[i] = a[2*i];
+        a1[i] = a[2*i+1];
+    }
+    fft(a0, invert);
+    fft(a1, invert);
+
+    double ang = 2 * PI / n * (invert ? -1 : 1);
+    cd w(1), wn(cos(ang), sin(ang));
+    for (int i = 0; 2 * i < n; i++) {
+        a[i] = a0[i] + w * a1[i];
+        a[i + n/2] = a0[i] - w * a1[i];
+        if (invert) {
+            a[i] /= 2;
+            a[i + n/2] /= 2;
+        }
+        w *= wn;
+    }
+}
+vector<int> multiply(vector<int> const& a, vector<int> const& b) {
+    vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+    int n = 1;
+    while (n < a.size() + b.size()) 
+        n <<= 1;
+    fa.resize(n);
+    fb.resize(n);
+
+    fft(fa, false);
+    fft(fb, false);
+    for (int i = 0; i < n; i++)
+        fa[i] *= fb[i];
+    fft(fa, true);
+
+    vector<int> result(n);
+    for (int i = 0; i < n; i++)
+        result[i] = round(fa[i].real());
+    return result;
+}
    
 int main() 
 { 
-    vector<complex<double> > a{1, 2, 3, 4}; 
+    vector<int > a ={1, 2, 3, 4}; 
     cout<<a[0]<<endl;
-    vector<complex<double> > b = fft(a); 
-    for (int i = 0; i < 4; i++) 
+    vector<int > b = {2, 3, 5, 6}; 
+    vector<int> result = multiply(a,b);
+    for (int i = 0; i < result.size(); i++) 
         cout << b[i] << endl; 
-  
     return 0;
 } 
